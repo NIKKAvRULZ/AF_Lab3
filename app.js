997c0611d16bcdf9fec474322e52f366bc9c6a07
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-03-01 09:12:12 +0530

    Replace HTTP server implementation with a console log of "Hello World"

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-const http = require('http');
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!');
-    res.end();
-}).listen(8080);
+console.log("Hello World");
